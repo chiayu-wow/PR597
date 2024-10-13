76f8534c60e8bb1d0023d847315ae45b0c87c39b
Yoyo <yoyowamg1012@gmail.com>
2024-10-13 17:51:06 -0500

    complete Hw5

---
+++ Hw4/test.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+array_3d_from_list = np.array(
+    [[1, 2, 3], [4, 5, 6]])
+
+array_3d_from_list[0,0] = 555
+
+
+print(array_3d_from_list)
---
+++ Hw5/hw5.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+data = pd.read_csv('data/VIW_FNT.csv')
+
+print(data)
